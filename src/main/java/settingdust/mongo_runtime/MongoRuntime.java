6f1f33a844840b8b1396d2737f7a8dd07170d362
SettingDust <settingdust@gmail.com>
2024-11-22 10:03:43 +0800

    fix: avoid transform classes above java 8

--- src/main/java/settingdust/mongo_runtime/MongoRuntime.java
+++ src/main/java/settingdust/mongo_runtime/MongoRuntime.java
@@ -1,6 +1,11 @@
 package settingdust.mongo_runtime;
 
+import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = "mongo_runtime", acceptableRemoteVersions = "*")
-public class MongoRuntime {}
+public class MongoRuntime {
+    public MongoRuntime() {
+        Launch.classLoader.addTransformerExclusion("com.mongodb.Jep395RecordCodecProvider");
+    }
+}
